1bb20861852aa7658a66d86f892c92bce593ba0b
Nurbek <nurbekizbassar@gmail.com>
2019-02-27 20:48:36 +0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Page from '../components/page';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+import Notification from '../components/notification';
+import { MediaQueryConsumer } from '../components/media-query';
+
+import Intro from '../components/home/intro';
+import Demo from '../components/home/demo';
+import Features from '../components/home/features';
+import Customers from '../components/home/customers';
+import Learn from '../components/home/learn';
+// import SocialMeta from '../components/social-meta';
+
+export default () => (
+  <Page title="White-cat | store">
+    {/* <SocialMeta
+      image="/static/twitter-cards/home.jpg"
+      title="Next.js - The React Framework"
+      url="https://nextjs.org"
+      description="Next.js is the React framework for production"
+    /> */}
+    <MediaQueryConsumer>
+      {({ isMobile }) => (
+        <Header height={32} offset={0} distance={0} shadow active={isMobile ? 32 : 160}>
+          {/* <Notification href="/blog/next-8" title="Next 8 is out!" titleMobile="Next 8 is out!">
+            Next 8 is out! — Serverless mode, performance and security improvements and more.
+            Monday, February 11th 2019
+          </Notification> */}
+          <Navbar hideLogo={!isMobile} />
+        </Header>
+      )}
+    </MediaQueryConsumer>
+    <Intro />
+    {/* <Demo /> */}
+    <Features />
+    <Customers />
+    <Learn />
+    <Footer />
+  </Page>
+);
